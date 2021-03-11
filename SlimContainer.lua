413cc55897983ac8381b1daa43449e5e6ed66966
codyorr4-roblox <60563146+codyorr4-roblox@users.noreply.github.com>
2021-03-11 17:47:59 -0600

    Added a .Dragged:Connect(f) event.

--- SlimContainer.lua
+++ SlimContainer.lua
@@ -9,20 +9,13 @@ local mouse = player:GetMouse()
     @Author - CodyOrr4
     @Date - 10/23/2020
     @Info - A module to help handle any GUI frame (or scroll frame) that will act as a 'container'
-    
-    @Notes:
-    1). I didn't think metatables were necassary for this module, let me know if they can be beneficial for the events.
-    2). button.LayoutOrder is considered the ID.
-    3). all container Guis must contain a list or grid layout instance.
-    4). the list/grid layout instance will determine the size and position of your button slots, so mess with the list/grid settings.
-	  5). All buttons must be Image Button instances.
 ]]
 
 
 
 
 
-function SlimContainer.new(guiObject, buttonTemplate, buttonAmount)
+function SlimContainer.new(guiObject)
 	
 	-- Create the 'Object'
 	local containerObject = {}
@@ -34,32 +27,31 @@ function SlimContainer.new(guiObject, buttonTemplate, buttonAmount)
 	containerObject.GuiObject = guiObject
 	containerObject.MouseButton1Down = {}
 	containerObject.MouseButton1Up = {}
+	containerObject.Dragging = {}
+	containerObject.DraggedObject = nil	
+	containerObject.Dragging = false
 	
 	
-	
-	
-	-- Optionally fill container up based on provided button template and amount
-	if(buttonTemplate and buttonAmount)then
-		for i = 1, buttonAmount do
-			local button = buttonTemplate:Clone()
-			button.LayoutOrder = i
-			button.Visible = true
-			button.Parent = guiObject
-		end
-	end
-	
-	
-	
+
 	
 	-- PRESSING
 	function containerObject.MouseButton1Down:Connect(f)
 		local con = uis.InputBegan:Connect(function(input, g)
-			if(g)then
+			
+			if(g and guiObject and guiObject.Visible)then
 				if(input.UserInputType == Enum.UserInputType.MouseButton1 and os.clock() >= containerObject.Cooldown)then
 					local s = playerGui:GetGuiObjectsAtPosition(mouse.X, mouse.Y)[1]
-					if(s and guiObject and s:IsA("ImageButton") and s.Parent.Name == guiObject.Name)then
+					
+					if(s and s:IsA("ImageButton") and s:IsDescendantOf(guiObject))then
 						containerObject.Cooldown = os.clock() + containerObject.CooldownInterval
-						f(s.LayoutOrder, s)
+
+						local holding = true
+						wait(0.2)
+						if(holding)then
+							containerObject.Dragging=true
+						else
+							f(s.LayoutOrder, s)
+						end
 					end
 				end
 			end
@@ -73,10 +65,12 @@ function SlimContainer.new(guiObject, buttonTemplate, buttonAmount)
 	-- RELEASING
 	function containerObject.MouseButton1Up:Connect(f)
 		local con = uis.InputEnded:Connect(function(input, g)
-			if(g)then
+			if((g or containerObject.Dragging) and guiObject.Visible)then
 				if(input.UserInputType == Enum.UserInputType.MouseButton1)then
-					local s = playerGui:GetGuiObjectsAtPosition(mouse.X, mouse.Y)[1]
-					if(s and guiObject and s:IsA("ImageButton") and s.Parent.Name == guiObject.Name)then
+					containerObject.Dragging=false
+					local s = playerGui:GetGuiObjectsAtPosition(mouse.X, mouse.Y)[containerObject.Dragging==false and 1 or 2]
+					
+					if(s and s:IsA("ImageButton") and s:IsDescendantOf(guiObject))then
 						f(s.LayoutOrder, s)
 					end
 				end
@@ -85,25 +79,25 @@ function SlimContainer.new(guiObject, buttonTemplate, buttonAmount)
 		return con
 	end
 	
+
 	
-	-- Get all buttons. (will not include the list/grid layout)
-	function containerObject:GetButtons()
-		local buttons = {}
-		local children = containerObject.GuiObject:GetChildren()
-		for i, v in ipairs(children)do
-			if(v:IsA("ImageButton"))then
-				table.insert(buttons,v)
+	-- DRAGGING
+	function containerObject.Dragging:Connect(f)
+		local origin = containerObject.GuiObject.AbsolutePosition
+		local con = uis.InputChanged:Connect(function(i,g)
+			if(g and i.UserInputType == Enum.UserInputType.MouseMovement and containerObject.Dragging)then
+				f(mouse.X-origin.X, mouse.Y-origin.Y)
 			end
-		end
-		return buttons
+		end)
+		return con
 	end
-	
 	
 	
 	-- Return the object created.
 	
 	return containerObject
 end
+
 
 
 
